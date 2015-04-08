aa553ea1b729e2fbcfe628b95012f2ad1ed89686
williamkennedy <williamkennedyjnr@gmail.com>
2015-04-08 09:54:47 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    #debugger
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params) #Not the final implemenation!
+    if @user.save
+      flash[:success] = "Welcome to the sample App"
+      redirect_to @user
+      # handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
